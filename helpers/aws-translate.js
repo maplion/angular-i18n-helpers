ce27f20eb0550a277522b9a8c852796f6e58cbf0
Saurabh Gaur <saurabh@eno8.com>
2021-07-26 21:42:15 +0530

    add functionality of generating xfl files

--- helpers/aws-translate.js
+++ helpers/aws-translate.js
@@ -2,50 +2,49 @@ const {
     TranslateClient,
     TranslateTextCommand
 } = require("@aws-sdk/client-translate");
+const {fromIni} = require("@aws-sdk/credential-provider-ini");
 
-const getClient = () => {
-    const client = new TranslateClient({
-        region: "us-east-1",
-    });
+const getClient = (profile) => {
+    // console.log("profile-->",profile)
+    let config={
+        region: "us-east-1"
+    }
+    if(profile){
+        // console.log("getClientgetClientgetClient-->",profile);
+        config.credentials=fromIni({profile:profile})
+    }
+    const client = new TranslateClient(config);
     return client;
 }
 
 // Translates some text into the Language of choice
 const translate = async (client, text, target) => {
-    console.log('TEXT :', text);
-    console.log('TEXT :', text);
-    console.log('TEXT :', text);
-    console.log('TEXT :', text);
-    console.log('TEXT :', text);
-    if (!text[0]) {
-        return ' ';
-    }
-    let translation;
     const params = {
         SourceLanguageCode: 'en',
         TargetLanguageCode: target,
-        Text: text[0]
+        Text: text
     }
     const command = new TranslateTextCommand(params);
-    return await new Promise(resolve => {
-        client.send(command)
-            .then(
-                (data) => {
-                    console.log(data);
-                    translation = data;
-                    resolve();
-                },
-                (error) => {
-                    console.log(error, error.stack);
-                }
-            ).catch(error => {
-                console.log('ERROR: ', error.message, error);
-            });
-    }).then(() => {
-        return translation;
-    }).catch(error => {
-        console.log('ERROR: ', error.message, error);
-    });
+    return client.send(command)
+    // return await new Promise(resolve => {
+    //     client.send(command)
+    //         .then(
+    //             (data) => {
+    //                 console.log(data);
+    //                 translation = data;
+    //                 resolve();
+    //             },
+    //             (error) => {
+    //                 console.log(error, error.stack);
+    //             }
+    //         ).catch(error => {
+    //             console.log('ERROR: ', error.message, error);
+    //         });
+    // }).then(() => {
+    //     return translation;
+    // }).catch(error => {
+    //     console.log('ERROR: ', error.message, error);
+    // });
 }
 
 module.exports = {
